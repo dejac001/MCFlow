2139117d4894133a06b464cd1b067ceed5039e53
Rob DeJaco <dejac001@umn.edu>
2019-09-25 22:18:40 +0000

    Fix units for internal energy

--- examples/clean_json_data_storage.py
+++ examples/clean_json_data_storage.py
@@ -1,5 +1,6 @@
 import json
 import math
+R = 8.314/1000.
 
 
 def clean_dG(file_name):
@@ -10,8 +11,8 @@ def clean_dG(file_name):
     for feed, val in data.items():
         try:
             clean_data[feed] = {
-                                'mean': val['prod-2']['1']['box2--box1']['mean'],
-                                'stdev': val['prod-2']['1']['box2--box1']['stdev']
+                                'mean': val['prod-2'][moltype]['box2--box1']['mean'],
+                                'stdev': val['prod-2'][moltype]['box2--box1']['stdev']
             }
         except KeyError:
             continue
@@ -24,20 +25,30 @@ def clean_dG(file_name):
         for key, val in clean_data.items():
             f.write('%s,%s,%e,%e\n'%(key.split('/')[0][:-1], key.split('/')[1], val['mean'], val['stdev']))
 
-def clean_U(file_name):
-    with open(file_name, 'r') as f:
+def clean_U(U_file_name, N_file_name):
+    with open(U_file_name, 'r') as f:
         data = json.load(f)
+    with open(N_file_name, 'r') as g:
+        N_data = json.load(g)
 
     clean_data = {}
     for feed, val in data.items():
         try:
+            N_box1_mean = N_data[feed]['prod-2'][moltype]['box1']['mean']
+            N_box2_mean = N_data[feed]['prod-2'][moltype]['box2']['mean']
+            N_box1_std = N_data[feed]['prod-2'][moltype]['box1']['stdev']
+            N_box2_std = N_data[feed]['prod-2'][moltype]['box2']['stdev']
             box1_mean = val['prod-2']['box1']['mean']
             box2_mean = val['prod-2']['box2']['stdev']
             box1_std  = val['prod-2']['box1']['mean']
             box2_std  = val['prod-2']['box2']['stdev']
-            clean_data[feed] = {
-                                'mean': (box1_mean - box2_mean)*8.314,
-                                'stdev': math.sqrt(box1_std*box1_std + box2_std*box2_std)*8.314
+            if N_box1_mean > 0. and N_box2_mean > 0.:
+                clean_data[feed] = {
+                                    'mean': (box1_mean/N_box1_mean - box2_mean/N_box2_mean)*R,
+                                    'stdev': math.sqrt(box1_std*box1_std/N_box1_mean/N_box1_mean + box2_std*box2_std/N_box2_mean/N_box2_mean
+    +N_box1_std*N_box1_std*(box1_mean/N_box1_mean/N_box1_mean)**2 +
+    N_box2_std*N_box2_std*(box2_mean/N_box2_mean/N_box2_mean)**2 
+    )*R
             }
         except KeyError:
             continue
@@ -50,6 +61,10 @@ def clean_U(file_name):
         for key, val in clean_data.items():
             f.write('%s,%s,%e,%e\n'%(key.split('/')[0][:-1], key.split('/')[1], val['mean'], val['stdev']))
 
+
+moltype='1'
+
+
 if __name__ == '__main__':
     clean_dG('dG-data.json')
-    clean_U('U-data.json')
+    clean_U('U-data.json', 'N-data.json')
