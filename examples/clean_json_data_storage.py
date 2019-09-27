3a1ba2bcfac8e1d5dff972d290f8762a40107fb6
Rob DeJaco <dejac001@umn.edu>
2019-09-27 18:49:52 +0000

    clean json data for dU calculation on fly

--- examples/clean_json_data_storage.py
+++ examples/clean_json_data_storage.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 import math
 R = 8.314/1000.
 
@@ -25,30 +26,18 @@ def clean_dG(file_name):
         for key, val in clean_data.items():
             f.write('%s,%s,%e,%e\n'%(key.split('/')[0][:-1], key.split('/')[1], val['mean'], val['stdev']))
 
-def clean_U(U_file_name, N_file_name):
-    with open(U_file_name, 'r') as f:
+def clean_dU(file_name):
+    with open(file_name, 'r') as f:
         data = json.load(f)
-    with open(N_file_name, 'r') as g:
-        N_data = json.load(g)
 
     clean_data = {}
     for feed, val in data.items():
+        if np.isnan(val['prod-2']['box2-->box1']['mean']):
+            continue
         try:
-            N_box1_mean = N_data[feed]['prod-2'][moltype]['box1']['mean']
-            N_box2_mean = N_data[feed]['prod-2'][moltype]['box2']['mean']
-            N_box1_std = N_data[feed]['prod-2'][moltype]['box1']['stdev']
-            N_box2_std = N_data[feed]['prod-2'][moltype]['box2']['stdev']
-            box1_mean = val['prod-2']['box1']['mean']
-            box2_mean = val['prod-2']['box2']['stdev']
-            box1_std  = val['prod-2']['box1']['mean']
-            box2_std  = val['prod-2']['box2']['stdev']
-            if N_box1_mean > 0. and N_box2_mean > 0.:
-                clean_data[feed] = {
-                                    'mean': (box1_mean/N_box1_mean - box2_mean/N_box2_mean)*R,
-                                    'stdev': math.sqrt(box1_std*box1_std/N_box1_mean/N_box1_mean + box2_std*box2_std/N_box2_mean/N_box2_mean
-    +N_box1_std*N_box1_std*(box1_mean/N_box1_mean/N_box1_mean)**2 +
-    N_box2_std*N_box2_std*(box2_mean/N_box2_mean/N_box2_mean)**2 
-    )*R
+            clean_data[feed] = {
+                                'mean': val['prod-2']['box2-->box1']['mean'],
+                                'stdev': val['prod-2']['box2-->box1']['stdev']
             }
         except KeyError:
             continue
@@ -62,9 +51,9 @@ def clean_U(U_file_name, N_file_name):
             f.write('%s,%s,%e,%e\n'%(key.split('/')[0][:-1], key.split('/')[1], val['mean'], val['stdev']))
 
 
-moltype='1'
+moltype='8'
 
 
 if __name__ == '__main__':
     clean_dG('dG-data.json')
-    clean_U('U-data.json', 'N-data.json')
+    clean_dU('dU-data.json')
