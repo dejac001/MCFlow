e275e39c145fd85f47d29c4b71716b01e79557e9
Rob DeJaco <dejac001@umn.edu>
2020-06-18 11:37:03 -0400

    Update test for changing probs

--- tests/change-probs/test.sh
+++
@@ -1,1 +0,0 @@
-python3 -O ../../changeProbs.py -f FAU-0 -i 1 2 -vv > out.log
--- tests/test_change_probs.py
+++ tests/test_change_probs.py
@@ -5,7 +5,10 @@ path = os.path.abspath(os.path.dirname(__file__))
 
 
 def test():
-    main(path=path, feeds=[os.path.join('change-probs', 'FAU-0')], verbosity=10, indep=(1, 2), debug=False)
+    main(path=os.path.join(path, 'change-probs'), feeds=[os.path.join('FAU-0')], verbosity=10, indep=(1, 2),
+         debug=False)
+    main(path=os.path.join(path, 'change-probs'), feeds=[os.path.join('FAU-0')], verbosity=10, indep=(1, 2),
+         debug=False)
 
 
 if __name__ == '__main__':
